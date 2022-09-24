7b0d92e6bd63345e884cf5d63f8ee6b9f5d7b76b
gustavo <gus09ayala@gmail.com>
2022-09-24 10:32:24 -0400

    Created factory and updated module Created factory for players and added function that changes player if no winner

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const gameBoardContainer = document.querySelector('.gameboard__container');
 
+
+const Player = (name, symbol) => {
+    const getName = () => name;
+    const getSymbol = () => symbol;
+    return { getName, getSymbol }
+}
+
+const player1 = Player('Player 1', 'X');
+const player2 = Player('Player 2', 'O');
+
 const gameBoard = (() => {
+    // Stores all plays for game
     const boardArray = [];
+    // Removes all tiles from DOM and empties boardArray
+    const clearBoard = () => {
+        while (gameBoardContainer.firstChild) {
+            gameBoardContainer.removeChild(gameBoardContainer.lastChild)
+        }
+        boardArray.splice(0)
+    }
+    // calls clearBoard and redraws DOM fill boardArray with empty strings
     const resetBoard = () => {
+        clearBoard();
         for (let i = 0; boardArray.length < 9; i++) {
             boardArray.push('')
             const gameBoardTile = gameBoardContainer.appendChild(document.createElement('div'));
@@ -21,13 +41,18 @@ const gameBoard = (() => {
 gameBoard.resetBoard()
 
 const gameFlow = (() => {
+    let currentPlayer = player1;
     const handlePlay = (tileClickIndex) => {
         if (gameBoard.boardArray[tileClickIndex] == '') {
-            gameBoard.boardArray.splice(tileClickIndex, 1, 'X')
+            gameBoard.boardArray.splice(tileClickIndex, 1, currentPlayer.getSymbol())
             console.log(gameBoard.boardArray);
             const clickedTile = document.querySelector(`.gameboard-tile[data-index="${tileClickIndex}"]`);
             clickedTile.textContent = gameBoard.boardArray[tileClickIndex];
-            checkWinner()
+            //checkWinner()
+            if (!checkWinner()) {
+                console.log('change player')
+                endTurn()
+            }
         } else {
             console.log('Whoops that place is taken!');
         }
@@ -52,8 +77,20 @@ const gameFlow = (() => {
             ((ar[2] === ar[5] && ar[2] != '') && (ar[5] === ar[8]) && ar[5] != '')
         ) {
             console.log('WINNER WINNER');
+            return true
+        }
+    }
+    const endTurn = () => {
+        console.log(`Current player is ${currentPlayer.getName()}`);
+        if (currentPlayer == player1) {
+            currentPlayer = player2
+            console.log(`Now its ${currentPlayer.getName()}s turn`);
+        } else {
+            currentPlayer = player1;
+            console.log(`Now its ${currentPlayer.getName()}s turn`);
         }
     }
     return { handlePlay }
 })()
+
 
